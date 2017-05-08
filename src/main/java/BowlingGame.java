c968f79d699ad52497ebd02d55529d70d3a1c531
twhomework <375859400@qq.com>
2017-05-08 12:44:37 +0800

    提交代码

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,84 @@
 public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
-        return 0;
+        int totalScore = 0;
+        for (int i = 0; i < bowlingCode.length(); i++){
+            if (bowlingCode.charAt(i) == '|' && bowlingCode.charAt(i+1) == '|') {
+                break;
+            }
+            if (bowlingCode.charAt(i) == '|') {
+                continue;
+            }
+            char score = bowlingCode.charAt(i);
+            if (score == 'X') {
+                totalScore += 10;
+                int add = 0;
+                int count = 0;
+                int index = i;
+                while (count < 2) {
+                    char temp = bowlingCode.charAt(++index);
+                    if (temp != '|'){
+                        if (temp == 'X') {
+                            add += 10;
+                            count++;
+                            continue;
+                        }
+                        if (temp == '/') {
+                            add = 10;
+                            count++;
+                            continue;
+                        }
+                        if (temp == '-') {
+                            count++;
+                            continue;
+                        } else {
+                            add += temp - '0';
+                            count++;
+                            continue;
+                        }
+                    }
+                }
+                totalScore += add;
+                continue;
+            }
+            if (score == '/') {
+                int pre = bowlingCode.charAt(i - 1) - '0';
+                totalScore = totalScore - pre + 10;
+                int add = 0;
+                int count = 0;
+                int index = i;
+                while (count < 1) {
+                    char temp = bowlingCode.charAt(++index);
+                    if (temp != '|'){
+                        if (temp == 'X') {
+                            add += 10;
+                            count++;
+                            continue;
+                        }
+                        if (temp == '-') {
+                            count++;
+                            continue;
+                        } else {
+                            add += temp - '0';
+                            count++;
+                            continue;
+                        }
+                    }
+                }
+                totalScore += add;
+                continue;
+            }
+            if (score == '-') {
+                continue;
+            }
+            else {
+                totalScore += score - '0';
+                continue;
+            }
+        }
+
+        return totalScore;
     }
+
+
 }
